629214e94685da96b5b33c366a327a03240eec82
rahulXs <rahulsharmaacn@gmail.com>
2018-06-10 20:49:15 +0530

    Bug fixes for proper noun and abbr.

--- dict.py
+++ dict.py
@@ -7,12 +7,18 @@ def dict_main(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     elif len(get_close_matches(word, data.keys()))>0:
-        choice = input("You mean %s? Y|y or N|n : " % get_close_matches(word, data.keys())[0])
-        if choice == "Y" or "y":
+        choice = input("You mean %s? y or n : " % get_close_matches(word, data.keys())[0])
+        if choice == "y":
             return data[get_close_matches(word, data.keys())[0]]
-        else:
+        elif choice == "n":
             return "Word not exits"
+        else:
+            return "Invalid entry"
 
     else:
         return "word not exits"
@@ -24,7 +30,6 @@ output = dict_main(word)
 if type(output) == list:
     for i in output:
         print("-->"+i)
-
 else:
     print(output)
 
